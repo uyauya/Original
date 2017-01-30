18986ba14377d2ea5222630ed379c08dde2c790a
満田俊介 <shunsuke.mitsuda>
2017-01-30 23:09:34 +0900

    カメラ修正

--- Assets/MapCreator.cs
+++ Assets/MapCreator.cs
@@ -5,8 +5,8 @@ public class MapCreator : MonoBehaviour {
 	public int				MAP_SIZE_X = 7;		// マップ横幅 (偶数指定の場合は、自動的に奇数にされる)
 	public int				MAP_SIZE_Z = 10;	// マップ奥幅 (偶数指定の場合は、自動的に奇数にされる)
 	private	GameObject		player;				// プレイヤーオブジェクト格納用
-	private MapSize		size;				// マップサイズ型の変数
-	private MapAxis		playerAxis;			// プレイヤー座標を扱うPlayerAxis型の変数
+	private MapSize		size;					// マップサイズ型の変数
+	private MapAxis		playerAxis;				// プレイヤー座標を扱うPlayerAxis型の変数
 	private MapArrayBlock	mapBlock;			// 地面用MapArrayBlock型の変数
 	private MapArrayFloor	mapFloor;			// 地上用MapArrayFloor型の変数
 	
@@ -16,38 +16,38 @@ public class MapCreator : MonoBehaviour {
 	public	GameObject[]	prefab_BreakBlock;
 	// ■■■■■■
 	void Start(){
-		player		= GameObject.FindGameObjectWithTag("Player");	// プレイヤーオブジェクト格納
+		player		= GameObject.FindGameObjectWithTag("Player");						// プレイヤーオブジェクト格納
 		
-		size		= new MapSize(MAP_SIZE_X , MAP_SIZE_Z);		// MapSizeクラスのインスタンス生成
+		size		= new MapSize(MAP_SIZE_X , MAP_SIZE_Z);								// MapSizeクラスのインスタンス生成
 		playerAxis	= new MapAxis(player , size , prefab_BL[0].transform.localScale);	// PlayerAxisクラスのインスタンス生成
-		mapBlock	= new MapArrayBlock(prefab_BL , "BL" , size , playerAxis);	// 地面用MapArrayクラスのインスタンス生成
-		mapFloor	= new MapArrayFloor("FL" , size , playerAxis);	// 地上用MapArrayクラスのインスタンス生成
+		mapBlock	= new MapArrayBlock(prefab_BL , "BL" , size , playerAxis);			// 地面用MapArrayクラスのインスタンス生成
+		mapFloor	= new MapArrayFloor("FL" , size , playerAxis);						// 地上用MapArrayクラスのインスタンス生成
 		
-		mapFloor.setWall(prefab_WALL);			// 壁オブジェクトの渡す
+		mapFloor.setWall(prefab_WALL);					// 壁オブジェクトの渡す
 		mapFloor.setObstacle(prefab_BreakBlock);		// 障害物オブジェクトを渡す
-		mapFloor.setEnemy(prefab_enemy);		// 敵オブジェクトを渡す
-		initialize();			// プレイヤー位置／マップ初期化
-		StartCoroutine("enemyEmitter" , 1.0f);	// 敵出現用のコルーチン開始
+		mapFloor.setEnemy(prefab_enemy);				// 敵オブジェクトを渡す
+		initialize();									// プレイヤー位置／マップ初期化
+		StartCoroutine("enemyEmitter" , 1.0f);			// 敵出現用のコルーチン開始
 	}
 
 	// ■■■プレイヤー位置／マップ初期化■■■
 	private void initialize(){
 		playerAxis.initialize();						// プレイヤーの初期座標を設定 (マップ中央座標)
-		mapBlock.startMap_Create();		// スタート時のマップ(床)作成
-		mapFloor.startMap_Create();		// スタート時のマップ(地上)作成
+		mapBlock.startMap_Create();						// スタート時のマップ(床)作成
+		mapFloor.startMap_Create();						// スタート時のマップ(地上)作成
 	}
 	
 	// ■■■■■■
 	void Update(){
-		playerAxis.updateAxis();	// プレイヤー座標を更新
-		mapBlock.renewal();			// マップ(床)の更新
-		mapFloor.renewal();			// マップ(地上)の更新
+		playerAxis.updateAxis();						// プレイヤー座標を更新
+		mapBlock.renewal();								// マップ(床)の更新
+		mapFloor.renewal();								// マップ(地上)の更新
 	}
 
 	// ■■■敵出現用のコルーチン■■■
 	IEnumerator enemyEmitter(float time){
 		while(true){
-			mapFloor.enemyArrival();	// 敵出現処理
+			mapFloor.enemyArrival();					// 敵出現処理
 			yield return new WaitForSeconds(time);		// time秒、処理を待機.
 		}
 	}
