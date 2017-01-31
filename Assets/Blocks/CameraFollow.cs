b8cd0a4c77621b850259e6dc3de54eff7845105a
満田俊介 <shunsuke.mitsuda>
2017-01-31 22:49:48 +0900

    １/31訂正

--- Assets/Blocks/CameraFollow.cs
+++ Assets/Blocks/CameraFollow.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class CameraFollow : MonoBehaviour 
 {
 	public Transform target;
-	public float smoothing = 5f;
+	//public float smoothing = 5f;
 	Vector3 offset;
 
 	/// <summary>
@@ -22,8 +22,9 @@ public class CameraFollow : MonoBehaviour
 	void LateUpdate ()
 	{
 		// カメラがターゲット（プレイヤー）を見つけてから追いかける（少し遅れて追いかける）
-		Vector3 targetCamPos = target.position + offset;
-		transform.position = Vector3.Lerp (transform.position, targetCamPos, smoothing * Time.deltaTime);
+		//Vector3 targetCamPos = target.position + offset;
+		//transform.position = Vector3.Lerp (transform.position, targetCamPos, smoothing * Time.deltaTime);
+		transform.position = target.position + offset;
 	}
 }
 
