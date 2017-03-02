1c7499a32ed15227feb0d634031a6cc5b8bba334
満田俊介 <shunsuke.mitsuda>
2017-03-02 22:17:07 +0900

    Boss出現条件追加

--- Assets/Scripts/MapCreator.cs
+++ Assets/Scripts/MapCreator.cs
@@ -4,17 +4,21 @@ using System.Collections;
 public class MapCreator : MonoBehaviour {
 	public int				MAP_SIZE_X = 7;		// マップ横幅 (偶数指定の場合は、自動的に奇数にされる)
 	public int				MAP_SIZE_Z = 10;	// マップ奥幅 (偶数指定の場合は、自動的に奇数にされる)
-	private	GameObject		player;				// プレイヤーオブジェクト格納用
+	public	GameObject		player;				// プレイヤーオブジェクト格納用
 	private MapSize		size;					// マップサイズ型の変数
 	private MapAxis		playerAxis;				// プレイヤー座標を扱うPlayerAxis型の変数
 	private MapArrayBlock	mapBlock;			// 地面用MapArrayBlock型の変数
 	private MapArrayFloor	mapFloor;			// 地上用MapArrayFloor型の変数
-	
+	public  GameObject[]    bossPrefab;
+	private GameObject[]	tagObjects;
 	public	GameObject[]	prefab_BL;			// 床ブロック格納用のプレファブ配列
 	public	GameObject[]	prefab_WALL;		// 壁ブロック格納用のプレファブ配列
 	public	GameObject[]	prefab_enemy;		// 敵の格納用のプレファブ配列
 	public	GameObject[]	prefab_BreakBlock;
-	// ■■■■■■
+	private float timer = 0.0f;
+	private float interval = 2.0f;
+
+	// ■■■
 	void Start(){
 		player		= GameObject.FindGameObjectWithTag("Player");						// プレイヤーオブジェクト格納
 		
@@ -39,9 +43,20 @@ public class MapCreator : MonoBehaviour {
 	
 	// ■■■■■■
 	void Update(){
-		playerAxis.updateAxis();						// プレイヤー座標を更新
-		mapBlock.renewal();								// マップ(床)の更新
-		mapFloor.renewal();								// マップ(地上)の更新
+		playerAxis.updateAxis ();						// プレイヤー座標を更新
+		mapBlock.renewal ();								// マップ(床)の更新
+		mapFloor.renewal ();								// マップ(地上)の更新
+		
+		timer += Time.deltaTime;
+		if (timer >= interval) {
+			Check ("GreenSphere");
+			timer = 0;
+		}
+	
+	}
+
+	void Check(string tagname){
+		tagObjects = GameObject.FindGameObjectsWithTag(tagname);
 	}
 
 	// ■■■敵出現用のコルーチン■■■
