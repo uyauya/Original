9b827066ca22035c7af2748bbfd2d8ac69ed1478
満田俊介 <shunsuke.mitsuda>
2017-04-01 14:25:36 +0900

    STAGE03作成開始、時間経過で暗くなるLightChange追加

---
+++ Assets/Scripts/Boss02Shot2.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss02Shot2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Destroy(gameObject, 4.0F);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/LightChange.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LightChange : MonoBehaviour {
+	public float count;
+	public Text timeLabel;
+	private GameObject lightMorning;
+	private GameObject lightEvening;
+	private GameObject lightNight;
+
+	// Use this for initialization
+	void Start () {
+		lightMorning = GameObject.Find ("LightMorning");
+		lightEvening = GameObject.Find ("LightEvening");
+		lightNight 	 = GameObject.Find ("Night");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		count += Time.deltaTime;
+		timeLabel.text = "" + count.ToString ("0");
+
+		if (count > 10) {
+			lightMorning.SetActive (false);
+		}
+		if (count > 20) {
+			lightEvening.SetActive (false);
+		}
+		if (count > 30) {
+			lightNight.SetActive (false);
+		}
+	}
+}
