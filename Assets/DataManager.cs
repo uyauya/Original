1feb5458a5c17fcd0b6f60aa87f3a7f2ef106c46
満田俊介 <shunsuke.mitsuda>
2017-05-14 19:25:58 +0900

    セレクト画面修正

--- Assets/DataManager.cs
+++ Assets/DataManager.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DataManager : MonoBehaviour {
-	public static int PlayerNo;
+	/*public static int PlayerNo;
 	// Use this for initialization
 	void Start () {
-		DontDestroyOnLoad(transform.gameObject);
+		DontDestroyOnLoad("Kohaku");
+		DontDestroyOnLoad("Yuko");
+		DontDestroyOnLoad("Misaki");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
-	}
+	}*/
 }
