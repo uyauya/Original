aa77639d39538da721663741bcf2184f4b3308cc
満田俊介 <shunsuke.mitsuda>
2017-06-02 00:10:19 +0900

    パラメータ更新6

--- Assets/Scripts/Boss02Shot.cs
+++ Assets/Scripts/Boss02Shot.cs
@@ -9,29 +9,33 @@ public class Boss02Shot : MonoBehaviour {
 	private Vector3 _dir;
 	private float _speed;
 	public GameObject SmallBoss02;
+	private float Interval=0;
 
 	// Use this for initialization
 	void Start () {
-		// スピードをランダムにする
-		float x = Random.Range (-0.5f,0.5f);
-		float y = Random.Range (-0.5f,0.5f);
-		float z = Random.Range (-0.5f,0.5f);
-		gameObject.transform.position = new Vector3 (x, y, z);
-		_speed = Random.Range (1, 9) / 10f;
-		// 方向をランダムにする
-		x = Random.Range (-0.1f, 0.1f);
-		y = Random.Range (-0.1f, 0.1f);
-		z = Random.Range (-0.1f, 0.1f);
-		_dir = new Vector3 (x, y, z);
-
-		//現後一定時間で自動的に消滅させる
-		Destroy(gameObject, 4.0F);
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (Interval > 0.5f) {
+			// スピードをランダムにする
+			float x = Random.Range (-0.5f,0.5f);
+			float y = Random.Range (-0.5f,0.5f);
+			float z = Random.Range (-0.5f,0.5f);
+			gameObject.transform.position = new Vector3 (x, y, z);
+			_speed = Random.Range (1, 9) / 10f;
+			// 方向をランダムにする
+			x = Random.Range (-0.1f, 0.1f);
+			y = Random.Range (-0.1f, 0.1f);
+			z = Random.Range (-0.1f, 0.1f);
+			_dir = new Vector3 (x, y, z);
+		}
+		Interval += Time.deltaTime;
+		//現後一定時間で自動的に消滅させる
+		Destroy(gameObject, 4.0F);
 		//弾を前進させる
-		//gameObject.transform.Translate (_dir * _speed);
+		gameObject.transform.Translate (_dir * _speed);
 	}
 
 	private void OnCollisionEnter(Collision collider) {
