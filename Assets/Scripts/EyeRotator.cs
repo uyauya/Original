3cfd2b876692b5a4a36826a6308504b814f9d8e6
満田俊介 <shunsuke.mitsuda>
2017-06-21 00:09:18 +0900

    EyeRotatorスクリプトでPlayerの目を固定（BassLayerのIkPassにチェック）

---
+++ Assets/Scripts/EyeRotator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class EyeRotator : MonoBehaviour 
+{
+	public Vector3 RotationL;
+	public Vector3 RotationR;
+	private Animator _animator;
+
+	// Use this for initialization
+	void Start () {
+		this._animator = this.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	private void OnAnimatorIK(int layerIndex) {
+		this._animator.SetBoneLocalRotation (HumanBodyBones.LeftEye, Quaternion.Euler (this.RotationL));
+		this._animator.SetBoneLocalRotation (HumanBodyBones.RightEye, Quaternion.Euler (this.RotationR));
+	}
+}
