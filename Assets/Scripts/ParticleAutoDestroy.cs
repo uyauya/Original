85008c6dc73e6d28962f0687872427b62c10045e
満田俊介 <shunsuke.mitsuda>
2017-06-28 23:28:36 +0900

    ショットエフェクト修正5

--- Assets/Scripts/ParticleAutoDestroy.cs
+++ Assets/Scripts/ParticleAutoDestroy.cs
@@ -3,12 +3,15 @@ using System.Collections;
 
 public class ParticleAutoDestroy : MonoBehaviour {
 
+	public float PDestroyTime;
+
 	// Use this for initialization
 	void Start () {
 	
 		//パーティクル終了時に自動的に消滅させる
 		ParticleSystem particleSystem = GetComponent <ParticleSystem>();
-		Destroy(gameObject, particleSystem.duration);
+		//Destroy(gameObject, particleSystem.duration);
+		Destroy(gameObject, PDestroyTime);
 	}
 	
 	// Update is called once per frame
