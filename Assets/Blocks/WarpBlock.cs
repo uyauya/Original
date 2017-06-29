5f7f8041480ee1faa11fd81b6e39da9284b7aa8f
満田俊介 <shunsuke.mitsuda>
2017-06-29 23:21:14 +0900

    微調整２

--- Assets/Blocks/WarpBlock.cs
+++ Assets/Blocks/WarpBlock.cs
@@ -7,7 +7,7 @@ public class WarpBlock : MonoBehaviour {
 	public float YPower;
 	public float ZPower;
 
-	void OnCollisionEnter(Collider other){
+	void OnTriggerEnter(Collider other){
 		if(other.gameObject.tag == "Player")
 		//other.gameObject.transform.position = new Vector3 (XPower, YPower, ZPower);
 			other.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(XPower, YPower, ZPower));
--- Assets/Scripts/PlayerShoot04.cs
+++ Assets/Scripts/PlayerShoot04.cs
@@ -62,8 +62,8 @@ public class PlayerShoot04 : MonoBehaviour {
 		if (Time.time - shotInterval > shotIntervalMax) {
 			shotInterval = Time.time;
 			GameObject bulletObject = GameObject.Instantiate (Bullet04)as GameObject;
-			bulletObject.transform.position = muzzle.position + transform.TransformDirection (Vector3.forward * 2);
-			bulletObject.GetComponent<Bullet01> ().damage = this.damage;
+			bulletObject.transform.position = muzzle.position + transform.TransformDirection(Vector3.forward * 2) + new Vector3(0, -0.3f, 0);
+			//bulletObject.GetComponent<Bullet01> ().damage = this.damage;
 		}
 	}
 
