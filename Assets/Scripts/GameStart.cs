f311e5645313156f7d97a5de84dedbea733d5178
shunsuke.mitsuda <mmdoyadoya@gmail.com>
2017-07-10 17:38:51 +0900

    音追加

--- Assets/Scripts/GameStart.cs
+++ Assets/Scripts/GameStart.cs
@@ -5,13 +5,15 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;		// UIを使う時は追加する
 
 public class GameStart : MonoBehaviour {
-
+	private AudioSource[] audioSources;
 	public Text blinkText;
 	public void Onclick_Select()
+
 	{
 		// ボタンを押してセレクト画面に移行
 		SceneManager.LoadScene("Select");
 		// テキスト点滅
 		blinkText.color = new Color(0, 0, 0, Mathf.PingPong(Time.time, 1));
+		audioSources [1].Play();
 	}
 }
