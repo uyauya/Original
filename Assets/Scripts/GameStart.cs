b8e707d8f4bd6f83c8efd549453075627b783bb9
満田俊介 <shunsuke.mitsuda>
2017-07-27 20:16:51 +0900

    スタート時点滅に変更

--- Assets/Scripts/GameStart.cs
+++ Assets/Scripts/GameStart.cs
@@ -6,8 +6,11 @@ using UnityEngine.UI;		// UIを使う時は追加する
 
 public class GameStart : MonoBehaviour {
 	private AudioSource[] audioSources;
+	//private Animator START;
+	private Animator animator;
 	public Text blinkText;
-	public void Onclick_Select()
+	public void Onclick_Select ()
+
 
 	{
 		// ボタンを押してセレクト画面に移行
@@ -15,19 +18,27 @@ public class GameStart : MonoBehaviour {
 		SoundManager.Instance.Play(1);	//(1)はElmentの数
 		Invoke("LoadScene",1.3f);
 		//SceneManager.LoadScene("Select");
-
-
 	}
+
+	void Awake(){
+		//START = GameObject.Find ("START").GetComponent<Animator> ();
+	}
+
 
 	void Start () {
 		audioSources = gameObject.GetComponents<AudioSource> ();
+		animator = GetComponent<Animator> ();
 		SoundManager.Instance.Play(0);
 	}
 
 	void Update () {
 		// テキスト点滅
-		if (Input.GetMouseButton(0)){
-			blinkText.color = new Color(0, 0, 0, Mathf.PingPong(Time.time * 10, 1f));
+		if (Input.GetMouseButtonDown(0)){
+			animator.SetBool ("START", true);
+			//blinkText.color = new Color(0, 0, 0, Mathf.PingPong(Time.time * 10, 1f));
+		} else {
+			animator.SetBool ("START", false);
+			//blinkText.color = new Color(0, 0, 0);
 		}
 	}
 
