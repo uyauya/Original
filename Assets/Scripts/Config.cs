5f5b9bfd625e5885da47d3efae4c8c6a6b226ff9
shunsuke.mitsuda <mmdoyadoya@gmail.com>
2017-09-21 19:47:00 +0900

    ポーズ修正２

--- Assets/Scripts/Config.cs
+++ Assets/Scripts/Config.cs
@@ -8,11 +8,25 @@ public class Config : MonoBehaviour {
 	private AudioSource[] audioSources;	
 	public void Onclick_Config ()
 	{
-		// ボタンを押してセレクト画面に移行
+		// ボタンを押してコンフィグ画面に移行
 		SoundManager.Instance.Play(1);	//(1)はElmentの数
 		//Invoke("LoadScene",0.3f);
 		SceneManager.LoadScene("Config");
 		//Debug.Log("コンフィグ");
+	}
+
+	public void Onclick_Exit ()
+	{
+		// ボタンを押してポーズ画面に移行
+		SoundManager.Instance.Play(1);	//(1)はElmentの数
+		SceneManager.LoadScene("Pause");
+	}
+
+	public void Onclick_End ()
+	{
+		// ボタンを押してスタート画面に移行
+		SoundManager.Instance.Play(1);	//(1)はElmentの数
+		SceneManager.LoadScene("Start");
 	}
 
 	void Start () {
