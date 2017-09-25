efeb4e9b73ca383df2ffde03b72568302254ce25
shunsuke.mitsuda <mmdoyadoya@gmail.com>
2017-09-25 18:59:08 +0900

    コンフィグ修正

--- Assets/Scripts/SelectEvent.cs
+++ Assets/Scripts/SelectEvent.cs
@@ -26,6 +26,43 @@ public class SelectEvent : MonoBehaviour {
 		Invoke("LoadScene3",1.3f);
 	}
 
+	public void Onclick_Config ()
+	{
+		// ボタンを押してコンフィグ画面に移行
+		SoundManager.Instance.Play(1);	//(1)はElmentの数
+		//Invoke("LoadScene",0.3f);
+		SceneManager.LoadScene("Config");
+		//Debug.Log("コンフィグ");
+	}
+
+	public void Onclick_Exit ()
+	{
+		// ボタンを押してポーズ画面に移行
+		SoundManager.Instance.Play(1);	//(1)はElmentの数
+		SceneManager.LoadScene("Start");
+	}
+
+	public void Onclick_End ()
+	{
+		// ボタンを押してスタート画面に移行
+		SoundManager.Instance.Play(1);	//(1)はElmentの数
+		SceneManager.LoadScene("Start");
+	}
+
+	public void Onclick_Save ()
+	{
+		// ボタンを押してセーブ画面に移行
+		SoundManager.Instance.Play(1);	//(1)はElmentの数
+		SceneManager.LoadScene("Start");
+	}
+
+	public void Onclick_Load ()
+	{
+		// ボタンを押してロード画面に移行
+		SoundManager.Instance.Play(1);	//(1)はElmentの数
+		SceneManager.LoadScene("Start");
+	}
+
 	void Start () {
 		SoundManager.Instance.Play(0);
 		SoundManager.Instance.Play(1);
