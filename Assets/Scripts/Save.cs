dfe2bf146432851a1507b559d47c324440f265d4
shunsuke.mitsuda <mmdoyadoya@gmail.com>
2017-10-20 18:58:22 +0900

    Saveスクリプト作成開始（未完成）

---
+++ Assets/Scripts/Save.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Save : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start()
+	{
+		//UserParamインスタンスを文字列に変換
+		UserParamSaveJson = JsonUtility.ToJson(UserParam);
+		//セーブ
+		PlayerPrefs.SetString("UserParam",UserParamSaveJson);
+		//ロード
+		string userParamLoadJson = PlayerPrefs.GetString("UserParam");
+		//データを変数に設定
+		UserParam = JsonUtility.FromJson<UserParam>(UserParamLoadJson);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void UserParamSaveJson()
+	{
+		
+}
