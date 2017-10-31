1666ed2702bbea1cf4a773a52ad9612ae9eeef11
shunsuke.mitsuda <mmdoyadoya@gmail.com>
2017-10-31 19:18:29 +0900

    細部調整

--- Assets/Scripts/Sample.cs
+++ Assets/Scripts/Sample.cs
@@ -4,14 +4,113 @@ using UnityEngine;
 
 public class Sample : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+		public GameObject bulletPrefab;
+		private GameObject hiyoko;
+		private bool isMovable = true;
+		private bool isJumpable = true;
+		private float velocityX = 0f; 
+
+		private void Start()
+		{
+			// ひよこちゃんを追っかけさせるため、ゲームオブジェクトを取得
+			hiyoko = GameObject.Find("Hiyoko");
+			StartCoroutine(ChaseCoroutine());
+			StartCoroutine(ShootIfPossibleCoroutine());
+		}
+		　　
+		private void Update()
+		{
+			// 移動させるための力を加える
+			var rigidbody = GetComponent<Rigidbody2D>();
+			rigidbody.velocity = new Vector2(isMovable ? velocityX : 0f, rigidbody.velocity.y);
+
+			// ジャンプ処理
+			if (hiyoko.transform.position.y > transform.position.y&& rigidbody.velocity.y == 0f)
+			{
+				　// ひよこちゃんが自分より高い位置に居るならジャンプする
+				　JumpIfPossible();
+			}
+		}
+
+		/// <summary>
+		/// ひよこちゃんを追っかけるためのコルーチンです。
+		/// </summary>
+		/// <returns>The coroutine.</returns>
+		private IEnumerator ChaseCoroutine()
+		{
+			while (true)
+			{
+				// ひよこちゃんが左右どちらに居るかをチェック、進行方 向を決めて追いかける
+				velocityX = hiyoko.transform.position.x > transform.position.x ? 0.5f : -0.5f;
+
+				// 次の方向転換は3秒後
+				yield return new WaitForSeconds(3f);
+			}
+		}
+
+		/// <summary>
+		/// 弾を発射するかどうか制御するコルーチンです。
+		/// </summary>
+		/// <returns>The coroutine.</returns>
+		private IEnumerator ShootIfPossibleCoroutine()
+		{
+			while (true)
+			{
+				if (Mathf.Abs(hiyoko.transform.position.x - transform.position.x) > 1.5f)
+				{
+					// ひよこちゃんと一定以上の距離が離れているならば、進行方向に向かって弾を発射
+					StartCoroutine(ShootBulletAndDestroyCoroutine());
+					// クールダウンで2秒立ち止まる
+					isMovable = false;
+					yield return new WaitForSeconds(2f);
+					isMovable = true ;
+				}
+				else
+				{
+					yield return new WaitForSeconds(0.1f);
+				}
+			}
+		}
+
+		/// <summary>
+		/// 弾発射から消滅までを制御するコルーチンです。
+		/// </ summary>
+		/// <returns>The bullet and destroy coroutine.</returns>
+		private IEnumerator ShootBulletAndDestroyCoroutine()
+		{
+			var bullet = Instantiate(bulletPrefab);
+			bullet.transform.position = transform.position + new Vector3(0f, 0.3f);
+			// 発射
+			bullet.GetComponent<Rigidbody2D>().velocity = new Vector2(velocityX* 3f, 5f); 
+			// 発射してから5秒待つ
+			yield return new WaitForSeconds(5f);
+			// 発射した弾を削除
+			Destroy(bullet);
+		}
+
+		/// <summary >
+		/// ジャンプ可能であればジャンプします。
+		/// </summary>
+		private void JumpIfPossible()
+		{
+			if (!isJumpable)
+			{
+				return;
+			}
+			var rigidbody = GetComponent<Rigidbody2D>();
+			rigidbody.AddForce( new Vector2(0f, 250f));
+			isJumpable = false;
+			StartCoroutine(JumpCooldownCoroutine());
+		}
+
+		/// <summary>
+		///ジャンプ後、次にジャンプ可能になるまでのクールダウンを制御します。
+		/// </summary>
+		/// <returns>The cooldown coroutine.</returns>
+		private IEnumerator JumpCooldownCoroutine()
+		{
+			yield return new WaitForSeconds(3f);
+			isJumpable = true;
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		// 上下させる
-		transform.position = new Vector3(transform.position.x, Mathf.PingPong(Time.time,10), transform.position.z);
-	}
-}
+
---
+++ Assets/Scripts/Sample2.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sample2 : MonoBehaviour 
+{
+
+
+	// 操作可能フラグ
+	private bool isControllable = true;
+
+	private void Update()
+	{ 
+		if (!isControllable)
+		{
+			return;
+		}
+
+		var rigidbody = GetComponent<Rigidbody2D>();
+		if (Input.GetAxis("Horizontal") < 0)
+		{
+			rigidbody.velocity = new Vector2(-1f, rigidbody.velocity.y);
+		}
+		else if (Input.GetAxis("Horizontal") > 0)
+		{
+			rigidbody.velocity = new Vector2(1f, rigidbody.velocity.y);
+		}
+		else
+		{
+			rigidbody.velocity = new Vector2(0f, rigidbody.velocity.y);
+		}
+
+		if (Input.GetButtonDown("Jump"))
+		{
+			rigidbody.AddForce(new Vector2(0f, 250f));
+		}
+	}
+
+	/// <summary>
+	/// 他のオブジェクトと衝突した際の処理
+	/// </summary>
+	/// <param name= "coll">Coll.</param>
+	private void OnCollisionEnter2D(Collision2D coll)
+	{
+		if ("Enemy" == coll.gameObject.tag)
+		{
+			Debug.Log("ここきてる？？");
+			StartCoroutine(CollisionToEnemyCoroutine(coll));
+		}
+	}
+
+	/// <summary>
+	/// Enemyと当たった時のリアクション
+	/// </summary>
+	/// <returns>The to enemy coroutine.</returns>
+	/// <param name="coll">Coll.</param>
+	private IEnumerator CollisionToEnemyCoroutine(Collision2D coll)
+	{
+		// 操作不能になる
+		isControllable = false;
+		Debug.Log("ここきてる？？");
+		var rigidbody = GetComponent<Rigidbody2D>();
+		// 吹き飛ばされる
+		rigidbody.AddForce(new Vector2(transform.position.x < coll.transform.position.x? -100f: 100f, 100f));
+		yield return new WaitForSeconds(0.5f);
+		// 0.5秒後、操作可能に
+		isControllable = true;
+	}
+}
+	
+
