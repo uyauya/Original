01b09e172f61b69da2bf1957bca00d6f3f4e2e3d
shunsuke.mitsuda <mmdoyadoya@gmail.com>
2017-11-09 19:31:14 +0900

    BossShot04作成開始

---
+++ Assets/Scripts/Blast.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// (rigidbodyの付いた）キャラを(放射状に）吹き飛ばす
+public class Blast : MonoBehaviour {
+	public float Pressure;   	// 圧力割合
+	public float speed;         // 爆風の速さ
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {		
+	}
+
+	void OnTriggerStay(Collider col) {
+		if ( col.GetComponent<Rigidbody>() == null ) {
+			return;
+		}
+
+		// 相対速度計算
+		var velocity = (col.transform.position - transform.position).normalized * speed;
+		// 空気抵抗を与える
+		col.GetComponent<Rigidbody>().AddForce(Pressure * velocity);
+	}
+
+}
---
+++ Assets/Scripts/Boss/Boss04Shot.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// (rigidbodyの付いた）キャラを押していくショット
+public class Boss04Shot : MonoBehaviour {
+
+	public float Pressure;   	// 圧力割合
+	public Vector3 velocity;	// 風速
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {		
+	}
+
+	void OnTriggerStay(Collider col) {
+		if ( col.GetComponent<Rigidbody>() == null ) {
+			return;
+		}
+
+		// 相対速度計算
+		var relativeVelocity = velocity - col.GetComponent<Rigidbody>().velocity;
+		// 空気抵抗を与える
+		col.GetComponent<Rigidbody>().AddForce(Pressure * relativeVelocity);
+	}
+
+}
