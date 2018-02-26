ced7194da2df69a5c65aad30e1182f2f28ca320e
満田俊介 <shunsuke.mitsuda>
2018-02-27 00:07:09 +0900

    細部調整

--- Assets/Scripts/System/SaveSlot.cs
+++ Assets/Scripts/System/SaveSlot.cs
@@ -5,10 +5,17 @@ using UnityEngine.SceneManagement;	// ステージ移行したい場合は追加
 using UnityEngine.UI;				// UIを使う時は追加する
 
 public class SaveSlot : MonoBehaviour {
+	public GameObject Dialogs;
+	//public Text text;
+	private int Number;
+	public void PushButton(int number) {
+		Number = number;
+		Dialogs.SetActive (true);
+		DialogSettings ();
+	}
 
-	public Text text;
-	public void OnClick(int number) {
-		switch (number) {
+	public void OnClick() {
+		switch (Number) {
 		case 0:
 			DataManager.Instance.SaveData("SaveSlot01");
 			break;
@@ -26,9 +33,17 @@ public class SaveSlot : MonoBehaviour {
 			break;
 		case 5:
 			DataManager.Instance.SaveData("SaveSlot06");
-			break;
+			break;;
 		default:
 			break;
 		}
 	}
+	public void DialogSettings(){
+		
+		Dialog.Message ("いいよ",OnClick,DialogNo);
+	}
+	public void DialogNo(){
+		Dialogs.SetActive (false);
+	}
+	YesOrNo Dialog = new YesOrNo ();	
 }
---
+++ Assets/Scripts/YesOrNo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class YesOrNo : MonoBehaviour {
+	public Text DialogMessage;
+	public GameObject YesButton;
+	public GameObject NoButton;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Message(string message,UnityAction Yes,UnityAction No) {
+		DialogMessage.text = message;
+		YesButton.GetComponent<EventYesOrNo> ().YesAction = Yes;
+		NoButton.GetComponent<EventYesOrNo> ().NoAction = No;
+	}
+}
