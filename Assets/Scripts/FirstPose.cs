2ac420a50274433a759b00016e13e4a3a06a62ac
満田俊介 <shunsuke.mitsuda>
2018-03-01 23:17:27 +0900

    細部調整

---
+++ Assets/Scripts/FirstPose.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPose : MonoBehaviour {
+
+	private Animator animator;	
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+		animator.SetTrigger ("Salute");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
