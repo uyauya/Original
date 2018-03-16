eebb3916032a1f3a079569bfe08892a1f0f59c5e
満田俊介 <shunsuke.mitsuda>
2018-03-17 00:25:33 +0900

    ゴースト作成（未完成）

--- Assets/Scripts/Enemy/DriftEnemy1.cs
+++ Assets/Scripts/Enemy/DriftEnemy1.cs
@@ -5,19 +5,23 @@ using UnityEngine;
 //フヨフヨ漂いながら移動する（収縮する）、ヒットさせるとしぼんでいく
 public class DriftEnemy1 : MonoBehaviour {
 	protected EnemyBasic enemyBasic;
-	public float Minimum = 1.0f;			// 最小（元の）サイズ
-	public float Magspeed = 10.0f;			// 拡大スピード
-	public float Magnification = 0.07f;		// 拡大率
+	public float Minimum = 150.0f;			// 最小（元の）サイズ
+	public float Magspeed = 0.01f;			// 拡大スピード
+	public float Magnification = 0.007f;		// 拡大率
+	public Vector3 BasicPoint;
 
 	void Start () {
 		enemyBasic = gameObject.GetComponent<EnemyBasic> ();
 		enemyBasic.Initialize ();
+		BasicPoint = new Vector3(this.transform.position.x, this.transform.position.y + 1.0f, this.transform.position.z);
 	}
 
 	void Update () {
-		this.transform.localScale = new Vector3(this.Minimum + Mathf.Sin(Time.time * this.Magspeed) * this.Magnification,
-			this.transform.localScale.y, this.Minimum + Mathf.Sin(Time.time * this.Magnification));
+		//this.transform.localScale = new Vector3(this.Minimum + Mathf.Sin(Time.time * this.Magspeed) * this.Magnification,
+			//Mathf.Sin(Time.time * this.Magspeed) * this.Magnification, Mathf.Sin(Time.time * this.Magspeed) * this.Magnification);
 		enemyBasic.timer += Time.deltaTime;
+		this.transform.position = new Vector3 (this.transform.position.x, this.BasicPoint.y + Mathf.Sin (Time.time), 
+			this.transform.position.z);
 		//敵の攻撃範囲を設定する
 		if (Vector3.Distance (enemyBasic.target.transform.position, transform.position) <= enemyBasic.TargetRange) {
 
@@ -31,21 +35,41 @@ public class DriftEnemy1 : MonoBehaviour {
 			transform.position += transform.up * Time.deltaTime * enemyBasic.JumpForce;
 			enemyBasic.timeElapsed = 0.0f;
 		}
-		// ターゲット（プレイヤー）との距離が0.5以内なら
+
+		// ターゲット（プレイヤー）との距離が5以内なら
+		//if (Vector3.Distance (enemyBasic.target.transform.position, transform.position) <= 5.0f) {
 		if (Vector3.Distance (enemyBasic.target.transform.position, transform.position) <= enemyBasic.Search) {
+			Debug.Log ("検出");
+			//if (Vector3.Distance (Player.target.transform.position, transform.position) <= enemyBasic.Search) {
 			//ターゲットの方を徐々に向く
 			// Quaternion.LookRotation(A位置-B位置）でB位置からA位置を向いた状態の向きを計算
 			// Quaternion.Slerp（現在の向き、目標の向き、回転の早さ）でターゲットにゆっくり向く
-			transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation
+			transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation 
+				(enemyBasic.target.transform.position - transform.position), Time.deltaTime * 5);
+			transform.position += transform.forward * Time.deltaTime * 20;
+			//一定間隔でショット
+			/*enemyBasic.shotInterval += Time.deltaTime;
+			// 次の攻撃待ち時間が一定以上になれば
+			if (enemyBasic.shotInterval > enemyBasic.shotIntervalMax) {
+				Instantiate (enemyBasic.shot, transform.position, transform.rotation);
+				enemyBasic.shotInterval = 0;
+			}*/			
+		}
+		/*if (Vector3.Distance (enemyBasic.target.transform.position, transform.position) <= enemyBasic.Search) {
+			//ターゲットの方を徐々に向く
+			// Quaternion.LookRotation(A位置-B位置）でB位置からA位置を向いた状態の向きを計算
+			// Quaternion.Slerp（現在の向き、目標の向き、回転の早さ）でターゲットにゆっくり向く
+			/*transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation
 				(enemyBasic.target.transform.position - transform.position), Time.deltaTime * enemyBasic.EnemySpeed);
 			transform.position += transform.forward * Time.deltaTime * 1;
 			if (enemyBasic.timeElapsed >= enemyBasic.timeOut) {
 				transform.position += transform.up * Time.deltaTime * enemyBasic.JumpForce;
 				enemyBasic.timeElapsed = 0.0f;
 			}
+
 			//animator.SetBool ("attack", true);
 			//Debug.Log ("hit");
-		}
+		}*/
 		// Animator の dead が true なら Update 処理を抜ける
 		//if( animator.GetBool("dead") == true ) return;
 	}
