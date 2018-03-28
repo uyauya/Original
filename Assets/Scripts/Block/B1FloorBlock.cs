4eced42120dfc5993c63e21994d73740f91d95a0
shunsuke.mitsuda <mmdoyadoya@gmail.com>
2018-03-28 19:17:27 +0900

    STAGE04用材料作成（未完成）

--- Assets/Scripts/Block/B1FloorBlock.cs
+++ Assets/Scripts/Block/B1FloorBlock.cs
@@ -2,7 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// プレイヤが接触したら落下するブロック
+// RigidBodyのisKinematicにチェックをいれておく
 public class B1FloorBlock : MonoBehaviour {
+	private ModelColorChange modelColorChange;
+	public float FalllTime = 0.5f;				//FloorBlockが始めるまでの時間
+	public float DestroyTime = 2.0f;			//消滅するまでの時間
+	public float FlashTime = 1.0f;				//点滅時間
+	public float WaitTime = 2.0f;				//B1FloorBlockが消え始めるまでの時間
 
 	// Use this for initialization
 	void Start () {
@@ -12,5 +19,43 @@ public class B1FloorBlock : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	// プレイヤタグの付いたオブジェクトが接触したらFallをFallTime後に起動
+	void OnCollisionEnter(Collision collision){
+		if(collision.gameObject.CompareTag("Player")){
+			Invoke("Fall", FalllTime);
+		}
+	}
+
+	// isKinematic(固定）のチェックを外して落下させ、DestroyTime後に消滅
+	void Fall(){
+		GetComponent<Rigidbody>().isKinematic = false;
+		StartCoroutine ("FloorDestroyCoroutine");
+	}
+
+	// 壁接触時の点滅
+	IEnumerator FloorDestroyCoroutine ()
+	{
+		//while文を10回ループ
+		int count = 4;
+		iTween.MoveTo(gameObject, iTween.Hash(
+			"time", FlashTime, // 好きな時間（秒）
+			"easetype", iTween.EaseType.linear
+		));
+		while (count > 0){
+			//数秒してから点滅開始
+			yield return new WaitForSeconds(WaitTime);
+			//透明にする
+			modelColorChange.ColorChange(new Color (1,0,0,1));
+			//0.1秒待つ
+			yield return new WaitForSeconds(0.1f);
+			//元に戻す
+			modelColorChange.ColorChange(new Color (1,1,1,1));
+			//0.1秒待つ
+			yield return new WaitForSeconds(0.1f);
+			count--;
+			Destroy(gameObject, DestroyTime);
+		}
 	}
 }
--- Assets/Scripts/System/MapCreator3.cs
+++ Assets/Scripts/System/MapCreator3.cs
@@ -4,40 +4,44 @@ using UnityEngine;
 
 public class MapCreator3 : MonoBehaviour {
 
-	public Transform blockPrefab;
-	public Transform topWall;   	//上の壁の参照を追加
-	public int placeX;         	 	//横に並べる個数
-	public int placeZ;          	//奥に並べる個数
-	public float totalDepth;    	//奥に並べる座標
+	public Transform BlockPrefab;
+	//public Transform BaseWall;   	//上の壁の参照を追加
+	public int PlaceX;         	 	//横に並べる個数
+	public int PlaceZ;          	//奥に並べる個数
+	public float TotalDepthX;    	//奥に並べる座標
+	public float TotalDepthZ;    	//奥に並べる座標
 
 	// Use this for initialization
 	void Start () {
 		//配置する座標を設定
-		Vector3 placePosition = new Vector3(
-			topWall.position.x-topWall.localScale.x/2+blockPrefab.localScale.x/2,
+		Vector3 placePosition = new Vector3(-1,-9,-1);
+		/*Vector3 placePosition = new Vector3(
+			BaseWall.position.x-BaseWall.localScale.x/2+BlockPrefab.localScale.x/2,
 			0,
-			topWall.position.z-topWall.localScale.z/2-blockPrefab.localScale.z/2);
+			BaseWall.position.z-BaseWall.localScale.z/2-BlockPrefab.localScale.z/2);*/
 
 		//配置する回転角を設定
 		Quaternion q = new Quaternion();
 		q= Quaternion.identity;				//回転なし
 
 		//幅と奥行きを調整
-		Vector3 localscale = blockPrefab.localScale;
-		localscale.x = topWall.localScale.x / placeX;
-		localscale.z = totalDepth / placeZ;
-		blockPrefab.localScale = localscale;
+		Vector3 localscale = BlockPrefab.localScale;
+		//localscale.x = BaseWall.localScale.x / PlaceX;
+		//localscale.z = TotalDepth / PlaceZ;
+		localscale.x = TotalDepthX / PlaceX;
+		localscale.z = TotalDepthZ / PlaceZ;
+		BlockPrefab.localScale = localscale;
 
 		//配置
-		for (int i = 0; i < placeZ; i++)
+		for (int i = 0; i < PlaceZ; i++)
 		{
 			Vector3 currentPlacePosition
 			= placePosition
-				- Vector3.forward * blockPrefab.localScale.z * i;
-			for(int j = 0; j < placeX; j++)
+				- Vector3.forward * BlockPrefab.localScale.z * i;
+			for(int j = 0; j < PlaceX; j++)
 			{
-				Instantiate(blockPrefab, currentPlacePosition, q);
-				currentPlacePosition.x += blockPrefab.transform.localScale.x;
+				Instantiate(BlockPrefab, currentPlacePosition, q);
+				currentPlacePosition.x += BlockPrefab.transform.localScale.x;
 			}
 		}
 	}
