717821ad34b2ff9562becb9c5a317733381d1028
shunsuke.mitsuda <shunsuke.mitsuda>
2018-05-06 23:30:22 +0900

    エキストラステージ作成開始

---
+++ Assets/PinBallStage/CloudController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudController : MonoBehaviour {
+
+	// 最小サイズ
+	public float Minimum = 1.0f;
+	// 拡大縮小スピード
+	public float MagSpeed = 10.0f;
+	// 拡大率
+	public float Magnification = 0.07f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// 雲を拡大縮小（X軸方向,Z軸方向に拡大縮小）
+		this.transform.localScale = new Vector3(this.Minimum + Mathf.Sin(Time.time * this.MagSpeed) * this.Magnification,
+			this.transform.localScale.y, this.Minimum + Mathf.Sin(Time.time * this.MagSpeed) * this.Magnification);
+	}
+}
