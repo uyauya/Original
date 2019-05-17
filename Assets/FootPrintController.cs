1f8b05429a7db14f994cafba898310a99d8b91ee
shunsuke.mitsuda <mmdoyadoya@gmail.com>
2019-05-17 12:18:19 +0900

    細部修正

---
+++ Assets/FootPrintController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootPrintController : MonoBehaviour
+{
+	void Start () {
+		StartCoroutine (Disappearing ());
+	}
+
+	IEnumerator Disappearing()
+	{
+		int step = 90;
+		for (int i = 0; i < step; i++)
+		{
+			GetComponent<MeshRenderer> ().material.color = new Color (1, 1, 1, 1 - 1.0f * i / step);
+			yield return null;
+		}
+		Destroy (gameObject);
+	}
+}
---
+++ Assets/PlayerFootPrint.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFootPrint : MonoBehaviour
+{
+	public GameObject footPrintPrefab;
+	float time = 0;
+
+	// Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+	void Update()
+	{
+		this.time += Time.deltaTime;
+		if (this.time > 0.35f)
+		{
+			this.time = 0;
+			Instantiate (footPrintPrefab, transform.position, transform.rotation);
+		}
+	}
+}
