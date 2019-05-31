75e2b1148dd46fec40da719dd64386cd75a69652
shunsuke.mitsuda <mmdoyadoya@gmail.com>
2019-05-31 23:22:15 +0900

    細部修正（未完成）

--- Assets/ParticleEquip.cs
+++ Assets/ParticleEquip.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class ParticleEquip : MonoBehaviour
 {
+    public TrailRenderer trailRenderer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.trailRenderer = GetComponent<TrailRenderer>();
+        trailRenderer.enabled = false;
     }
 
     // Update is called once per frame
