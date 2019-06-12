23947307448e94ba5b21d330f101bdef85efc36d
shunsuke.mitsuda <mmdoyadoya@gmail.com>
2019-06-12 18:56:50 +0900

    細部修正（未完成）

---
+++ Assets/ParticleEquip.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleEquip : MonoBehaviour
+{
+	public static bool ParticleOn = false;
+	public GameObject Particle01;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		//TrailOnの状態時、trailRenderer使用可とする
+		if(ParticleOn == true)
+		{
+			Particle01.SetActive(true);
+		}
+		else if(ParticleOn == false)
+		{
+			Particle01.SetActive(false);
+		}
+	}
+}
